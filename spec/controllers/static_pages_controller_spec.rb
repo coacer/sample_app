d7b494326918525830486112f033005133fa0b0b
coacer <coacer728h23@icloud.com>
2019-07-04 02:11:56 +0900

    Finish static pages

---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe StaticPagesController, type: :controller do
+  render_views
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
+  describe "#home" do
+    it "should get home" do
+      get :home
+      expect(response).to be_successful
+      # assert_select "title", "home | #{base_title}"
+      expect(response.body).to have_title("Home | #{base_title}")
+    end
+  end
+
+  describe "#help" do
+    it "should get help" do
+      get :help
+      expect(response).to be_successful
+      # assert_select "title", "Help | #{base_title}"
+      expect(response.body).to have_title("Help | #{base_title}")
+    end
+  end
+
+  describe "#about" do
+    it "should get about" do
+      get :about
+      expect(response).to be_successful
+      # assert_select "title", "About | #{base_title}"
+      expect(response.body).to have_title("About | #{base_title}")
+    end
+  end
+
+  describe "#contact" do
+    it "should get contact" do
+      get :contact
+      expect(response).to be_successful
+      expect(response.body).to have_title("Contact | #{base_title}")
+    end
+  end
+
+end
